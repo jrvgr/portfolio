85902162152d122aa7d6a15c00d26097c5e7060c
viriv <viriv@protonmail.com>
2021-06-20 22:37:34 +0200

    small changes

--- index.js
+++ index.js
@@ -5,8 +5,10 @@ function hideSidebar() {
         document.body.style.overflowX = ""
 
 
-    } else { sidebar.classList.add("hidden") 
-    document.body.style.overflowX = "hidden"}
+    } else {
+        sidebar.classList.add("hidden")
+        document.body.style.overflowX = "hidden"
+    }
 }
 
 
@@ -15,16 +17,16 @@ function toggledark() {
     const sidebar = document.getElementsByClassName("sidebar")[0];
     const darkmodeToggle = document.querySelector('.darktoggle')
     const image = document.querySelector(".borger")
-    
+
     image.src = "images/Hamburger_icon_dark.svg"
     darkmodeToggle.innerText = "Dark mode"
     body.classList.toggle("isdark");
-    
+
     if (body.classList.contains("isdark")) {
         darkmodeToggle.innerText = "Light mode";
     }
 
-    
+
 
 }
 
