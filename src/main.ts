506cc9b7f07643a82f35731462a3c9f1f5ecd5cd
viriv <viriv@protonmail.com>
2021-10-16 14:10:04 +0200

    lots of fixes and change to ts with cleaner syntax

--- src/main.ts
+++ src/main.ts
@@ -1,2 +1,24 @@
 import './style.css'
 
+
+
+const darkmode = function() {
+  const darkmode:HTMLSpanElement = document.querySelector('.darkmode .navtext')!
+
+  document.body.classList.toggle('dark');
+  if (darkmode.innerText === 'Dark Mode'){
+    darkmode.innerText = 'Light Mode'
+    localStorage.setItem('darkmode','true')}
+  else {
+    darkmode.innerText = 'Dark Mode'
+    localStorage.clear}
+} 
+
+document.querySelector('.darkmode')!.addEventListener('click', darkmode)
+
+const restore = function () {
+if (localStorage.getItem('darkmode') === 'true') {
+  darkmode()}
+}
+
+restore()
