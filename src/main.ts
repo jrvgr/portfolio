eb39575f9f337aebd9712d484524d9ebbb99a601
viriv <viriv@protonmail.com>
2022-02-04 18:49:01 +0100

    initial commit

---
+++ src/main.ts
@@ -0,0 +1,32 @@
+//boilerplating
+import "./styling/style.scss";
+
+import { createIcons, icons } from "lucide";
+createIcons({ icons });
+
+function darkToggle(storage: Storage): void {
+	const { classList } = document.body;
+	classList.toggle("dark");
+	storage.setItem("dark", classList.contains("dark").toString());
+}
+
+onload = function () {
+	{
+		document
+			.querySelector(".darkToggle")
+			?.addEventListener("click", () => darkToggle(localStorage));
+	}
+
+	if (localStorage.getItem("dark") === "true") {
+		darkToggle(localStorage);
+	}
+
+	const styles = getComputedStyle(document.documentElement);
+	const bgColor = styles.getPropertyValue("--prefers-dark");
+
+	console.log(bgColor);
+
+	if (bgColor === "true" && !localStorage.getItem("dark")) {
+		darkToggle(localStorage);
+	}
+};
