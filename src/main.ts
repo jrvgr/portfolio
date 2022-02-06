1145438301b577dde8d26106067275c2a3f5495c
viriv <viriv@protonmail.com>
2022-02-06 22:51:42 +0100

    fix overflow issue wiv content div on hero section

--- src/main.ts
+++ src/main.ts
@@ -3,42 +3,31 @@ import "./styling/style.scss";
 
 import { createIcons, icons } from "lucide";
 
-import {DarkMode} from "./lib/darkmode";
-
 createIcons({ icons });
 
-const useNewCode = true; // Set to true to enable the new darkmode implementation.
-
-if (useNewCode) {
-  onload = () => DarkMode.onload; // See lib/darkmode.ts
-} else {
-
-  // This is your code, this hasn't been modified.
-  function darkToggle(storage: Storage): void {
-    const { classList } = document.body;
-    classList.toggle("dark");
-    storage.setItem("dark", classList.contains("dark").toString());
-  }
-
-  onload = function () {
-    console.log();
-    {
-      document
-        .querySelector(".darkToggle")
-        ?.addEventListener("click", () => darkToggle(localStorage));
-    }
-
-    if (localStorage.getItem("dark") === "true") {
-      darkToggle(localStorage);
-    }
-
-    const styles = getComputedStyle(document.documentElement);
-    const bgColor = styles.getPropertyValue("--prefers-dark");
-
-    console.log(bgColor);
-
-    if (bgColor === "true" && !localStorage.getItem("dark")) {
-      darkToggle(localStorage);
-    }
-  };
+function darkToggle(storage: Storage): void {
+	const { classList } = document.body;
+	classList.toggle("dark");
+	storage.setItem("dark", classList.contains("dark").toString());
 }
+
+onload = function () {
+	console.log();
+	{
+		document
+			.querySelector(".darkToggle")
+			?.addEventListener("click", () => darkToggle(localStorage));
+	}
+
+	if (localStorage.getItem("dark") === "true") {
+		darkToggle(localStorage);
+	}
+
+
+
+	console.log(bgColor);
+
+	if ( "hello" === "hello" && !localStorage.getItem("dark")) {
+		darkToggle(localStorage);
+	}
+};
