918f445b30271b220454c575cd87ccf5f205381e
Viriv <viriv@protonmail.com>
2023-04-02 13:56:16 +0200

    remove test data and update github endpoint

--- src/api/github.ts
+++ src/api/github.ts
@@ -1,10 +1,8 @@
 import type { StarredProject } from "./github.d";
-import testData from "./testdata.json";
 
 export async function getProjects() {
-  const response = await fetch("https://api.github.com/users/im-viriv/starred");
+  const response = await fetch("https://api.github.com/users/J-Groen/starred");
   const data: StarredProject[] = await response.json();
-  // const data = testData as unknown as StarredProject[];
   return data.filter(
     (project) => project.owner.id === 30769091
   ) as StarredProject[];
