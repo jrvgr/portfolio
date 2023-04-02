3d049c7331fcdb538d7840e69ec04aec774e8164
Viriv <viriv@protonmail.com>
2023-04-03 00:42:53 +0200

    update vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,11 @@
 import { defineConfig } from "vite";
 import { svelte } from "@sveltejs/vite-plugin-svelte";
 import Unfonts from "unplugin-fonts/vite";
-import { spaLoading } from "vite-plugin-spa-loading";
 import Icons from "unplugin-icons/vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
-    // spaLoading("img", {
-    //   tipText: "Hold your horses, I'm here in a second!",
-    //   src: "/loading.svg",
-    //   css: "user-select: none, z-index: -1; pointer-events: none;",
-    //   debounce: 150,
-    // }),
     Unfonts({
       google: {
         families: ["Inter", "Unbounded", "Corben"],
