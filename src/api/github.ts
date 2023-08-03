ac5ca83d76f7394021a020858e48583e6b30af66
Jacco Groen <viriv@protonmail.com>
2023-08-04 00:25:11 +0200

    replace github api call too

--- src/api/github.ts
+++ src/api/github.ts
@@ -2,7 +2,7 @@ import type { StarredProject } from "./github.d";
 export type { StarredProject } from "./github.d";
 
 export async function getProjects() {
-  const response = await fetch("https://api.github.com/users/J-Groen/starred");
+  const response = await fetch("https://api.github.com/users/jrvgr/starred");
   const data: StarredProject[] = await response.json();
   return data
     .filter((project) => project.owner.id === 30769091)
